77f41c01ea0d34f395dd9eae6a6aa9cbed68b2d6
Yali <oferyali@gmail.com>
2019-11-13 19:41:07 +0200

    Tryouts

--- app/src/main/java/com/example/travelog/MainActivity.java
+++ app/src/main/java/com/example/travelog/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient locationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
     }
 }
